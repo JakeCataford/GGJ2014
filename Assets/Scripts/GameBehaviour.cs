7900249378b962dfafe20025e69c2f022eb1a7ae
JakeCataford <JakeCataford@Gmail.com>
2014-01-24 19:35:13 -0500

    Game Manager Implementation and behaviour class

---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBehaviour : MonoBehaviour {
+	public GameManager Game {
+		get {
+			return GameManager.Instance;
+		}
+		private set {}
+	}
+}
