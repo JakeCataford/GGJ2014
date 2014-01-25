8244e31f0080b31e2df04eac86b4b8d1991b7e0a
Sam Dye <sam.re.dye@gmail.com>
2014-01-25 14:44:50 -0500

    Add Hopper enemy base

---
+++ Assets/Scripts/HopperEnemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HopperEnemy : Enemy {
+
+	void Start () {
+		health = 10;
+		attackDamage = 10;
+		movementSpeed = 10f;
+	}
+	
+	void Update () {
+	
+	}
+}
