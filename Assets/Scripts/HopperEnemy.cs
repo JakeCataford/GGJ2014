04693e0ff8bd63104099b3516e7fb66c0ffd4b69
Sam Dye <sam.re.dye@gmail.com>
2014-01-25 17:14:55 -0500

    Hopping properly

--- Assets/Scripts/HopperEnemy.cs
+++ Assets/Scripts/HopperEnemy.cs
@@ -5,32 +5,39 @@ using CollisionExtentions;
 public class HopperEnemy : Enemy {
 
 	private bool grounded;
-	private Vector2 debugLastNormal;
+	private Vector2 debugLastNormal = new Vector2(0, 1);
 
 	void Start () {
-		enemyAI = new BaseAI();
+		rigidbody2D.fixedAngle = true;
+		enemyAI = GetComponent<BaseAI>();
+		if (!enemyAI) {
+			enemyAI = gameObject.AddComponent<BaseAI>();
+		}
 		health = 10;
 		attackDamage = 10;
-		movementSpeed = 3f;
+		movementSpeed = 10f;
 	}
 	
 	void Update () {
 		if (isVisible && enemyAI.currentState == BaseAI.State.ENGAGED) {
 			attackPlayer();
 		}
+		grounded = false;
 	}
 
 	void OnCollisionStay2D(Collision2D col) {
-		Vector2 collisionAverageNormal = col.contacts.AverageNormal ();
-		debugLastNormal = collisionAverageNormal;
-		if (Vector2.Dot (collisionAverageNormal, Vector2.up) > 0.9f) {
-			grounded = true;
+		if (col.collider.tag != "Player") {
+			Vector2 collisionAverageNormal = col.contacts.AverageNormal ();
+			debugLastNormal = collisionAverageNormal;
+			if (Vector2.Dot (collisionAverageNormal, Vector2.up) > 0.9f) {
+				grounded = true;
+			}
 		}
 	}
 
 	void attackPlayer() {
 		if (grounded) {
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(transform.position.x - enemyAI.targetPlayer.transform.position.x) * movementSpeed, movementSpeed);
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(enemyAI.targetPlayer.transform.position.x - transform.position.x) * movementSpeed, movementSpeed);
 		}
 	}
 
