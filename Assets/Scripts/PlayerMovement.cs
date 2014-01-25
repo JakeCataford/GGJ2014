53b24d760dffd265c59b7834489d0487ded8e85b
Sam Dye <sam.re.dye@gmail.com>
2014-01-25 03:57:35 -0500

    Add player Boost

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,27 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	[HideInInspector]
-	public bool facingRight = true;			// For determining which way the player is currently facing.
+	public bool facingRight = true;
 	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
+	public bool jump = false;
 	[HideInInspector]
 	public bool boost = false;
 	[HideInInspector]
 	public bool hang = false;
-	
-	public float moveDecelerationLerpTime = 1f;	// Amount of force added to move the player left and right.
-	public float speed = 2f;				// The fastest the player can travel in the x axis.
-	public float jumpForce = 250f;			// Amount of force added when the player jumps.
+
+	public float boostDecelerationLerpTime = 0.02f;
+	public float moveDecelerationLerpTime = 1f;	
+	public float playerSpeed = 2f;
+	public float jumpForce = 250f;
+	public float boostForce = 500f;
 
 	public bool hasPowerBooster = false;
 	public bool hasPowerWallClimb = false;
 
-	private bool grounded = false;			// Whether or not the player is grounded.
+	private bool grounded = false;
 	private bool leftCollision = false;
 	private bool rightCollision = false;
+	private bool powerBoosterUsed = false;
 
 	void Update()
 	{
@@ -31,12 +34,14 @@ public class PlayerMovement : MonoBehaviour {
 		leftCollision = Physics2D.Raycast(transform.position, -Vector2.right, ((BoxCollider2D)collider2D).size.y / 1.9f, 1 << LayerMask.NameToLayer("World"));
 		rightCollision = Physics2D.Raycast(transform.position, Vector2.right, ((BoxCollider2D)collider2D).size.y / 1.9f, 1 << LayerMask.NameToLayer("World"));
 
-		if(Input.GetButtonDown("Jump") && grounded) {
-			jump = true;
+		if (grounded) {
+			powerBoosterUsed = false;
 		}
 
-		if (Input.GetButton("Boost") && grounded) {
-			boost = true;
+		if(Input.GetButtonDown("Jump") && grounded) {
+			jump = true;
+		} else if (Input.GetButton("Fire2") && hasPowerBooster && powerBoosterUsed == false) {
+				boost = true;
 		}
 	}
 	
@@ -45,17 +50,22 @@ public class PlayerMovement : MonoBehaviour {
 	{
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis ("Vertical");
-		
-		if (h == 0) {
-			rigidbody2D.velocity = new Vector2(Mathf.Lerp(rigidbody2D.velocity.x, 0f, moveDecelerationLerpTime), rigidbody2D.velocity.y);
+
+//		Horizontal movement
+		if (powerBoosterUsed) {
+			rigidbody2D.velocity = new Vector2(Mathf.Lerp(rigidbody2D.velocity.x, 0f, boostDecelerationLerpTime), rigidbody2D.velocity.y);
 		} else {
-			if ((h > 0 && !rightCollision) || (h < 0 && !leftCollision)) {
-				rigidbody2D.velocity = new Vector2(Mathf.Sign(h) * speed, rigidbody2D.velocity.y);
+			if (h == 0) {
+				rigidbody2D.velocity = new Vector2(Mathf.Lerp(rigidbody2D.velocity.x, 0f, moveDecelerationLerpTime), rigidbody2D.velocity.y);
 			} else {
-				rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
+				if ((h > 0 && !rightCollision) || (h < 0 && !leftCollision)) {
+					rigidbody2D.velocity = new Vector2(Mathf.Sign(h) * playerSpeed, rigidbody2D.velocity.y);
+				} else {
+					rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
+				}
 			}
 		}
-		
+
 		if (h > 0 && !facingRight) {
 			Flip();
 		} else if (h < 0 && facingRight) {
@@ -63,8 +73,11 @@ public class PlayerMovement : MonoBehaviour {
 		}
 		
 		if (jump) {
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));			
-			jump = false;
+			Jump ();
+		}
+
+		if (boost) {
+			Boost (h, v);
 		}
 	}
 	
@@ -77,8 +90,29 @@ public class PlayerMovement : MonoBehaviour {
 		transform.localScale = theScale;
 	}
 
-	void Boost (Vector2 vector)
+	void Jump ()
 	{
+		rigidbody2D.AddForce(new Vector2(0f, jumpForce));			
+		jump = false;
+	}
 
+	void Boost (float xInput, float yInput)
+	{
+		float boostX = 0, boostY = 0;
+
+		if (yInput == 0 && xInput == 0) {
+			boostY = 1;
+		} else {
+			if (xInput != 0) {
+				boostX = xInput > 0 ? 1 : -1;
+			}
+			if (yInput != 0) {
+				boostY = yInput > 0 ? 1 : -1;
+			}
+		}
+		
+		rigidbody2D.AddForce(new Vector2(boostX, boostY).normalized * boostForce);
+		boost = false;
+		powerBoosterUsed = true;
 	}
 }
