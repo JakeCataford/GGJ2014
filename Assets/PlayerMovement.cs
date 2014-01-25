84832058e698b7539f4c546f9703d8261b788ddc
Sam Dye <sam.re.dye@gmail.com>
2014-01-24 21:34:16 -0500

    Improved movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,8 +10,8 @@ public class PlayerMovement : MonoBehaviour {
 	[HideInInspector]
 	public bool jump = false;				// Condition for whether the player should jump.
 	
-	public float moveDeceleration = 100f;	// Amount of force added to move the player left and right.
-	public float maxSpeed = 0.05f;			// The fastest the player can travel in the x axis.
+	public float moveDecelerationLerpTime = 1f;	// Amount of force added to move the player left and right.
+	public float speed = 2f;				// The fastest the player can travel in the x axis.
 	public float jumpForce = 250f;			// Amount of force added when the player jumps.
 	
 	private bool grounded = false;			// Whether or not the player is grounded.
@@ -21,9 +21,9 @@ public class PlayerMovement : MonoBehaviour {
 	void Update()
 	{
 		// The player is grounded if a raycast from the transform position collides with anything
-		grounded = Physics2D.Raycast(transform.position, -Vector2.up, ((BoxCollider2D)collider2D).size.y / 1.5f, 1 << LayerMask.NameToLayer("World"));
-		leftCollision = Physics2D.Raycast(transform.position, -Vector2.right, ((BoxCollider2D)collider2D).size.y / 1.5f, 1 << LayerMask.NameToLayer("World"));
-		rightCollision = Physics2D.Raycast(transform.position, -Vector2.right, ((BoxCollider2D)collider2D).size.y / 1.5f, 1 << LayerMask.NameToLayer("World"));
+		grounded = Physics2D.Raycast(transform.position, -Vector2.up, ((BoxCollider2D)collider2D).size.y / 1.9f, 1 << LayerMask.NameToLayer("World"));
+		leftCollision = Physics2D.Raycast(transform.position, -Vector2.right, ((BoxCollider2D)collider2D).size.y / 1.9f, 1 << LayerMask.NameToLayer("World"));
+		rightCollision = Physics2D.Raycast(transform.position, Vector2.right, ((BoxCollider2D)collider2D).size.y / 1.9f, 1 << LayerMask.NameToLayer("World"));
 
 		// If the jump button is pressed and the player is grounded then the player should jump.
 		if(Input.GetButtonDown("Jump") && grounded)
@@ -37,14 +37,16 @@ public class PlayerMovement : MonoBehaviour {
 		float h = Input.GetAxis("Horizontal");
 		
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rigidbody2D.velocity.x < maxSpeed)
-			// ... add a force to the player.
-			rigidbody2D.AddForce(Vector2.right * h * moveForce);
-		
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+		if (h == 0) {
+			rigidbody2D.velocity = new Vector2(Mathf.Lerp(rigidbody2D.velocity.x, 0f, moveDecelerationLerpTime), rigidbody2D.velocity.y);
+		} else {
+			if ((h > 0 && !rightCollision) || (h < 0 && !leftCollision)) {
+				rigidbody2D.velocity = new Vector2(Mathf.Sign(h) * speed, rigidbody2D.velocity.y);
+			} else {
+				rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
+			}
+		}
 		
 		// If the input is moving the player right and the player is facing left...
 		if(h > 0 && !facingRight)
